85dc05572f8b2de28258c3a070b54213c6ca4537
Datravous Odds <datravousodds@gmail.com>
2025-05-29 17:39:33 -0500

    Update index.html, styles.css, and main.js to enhance contact modal functionality and form validation. Changed navigation links for consistency, improved form input styles, and added JavaScript for error handling and modal interactions, ensuring a better user experience.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,75 @@
-console.log("Hello World");
+// Contact Modal Functionality
+const contactBtn = document.getElementById("contact-btn");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
+
+// Feature Filters Functionality
+const filterList = document.querySelector(".filter-list");
+
+// Form Validation
+const modalOverlay = document.querySelector(".modal-overlay");
+
+const formInput = {
+  name: document.getElementById("contact-name"),
+  email: document.getElementById("contact-email"),
+  subject: document.getElementById("contact-subject"),
+  message: document.getElementById("contact-message"),
+};
+
+const formErrors = {
+  name: document.getElementById("name-error"),
+  email: document.getElementById("email-error"),
+  subject: document.getElementById("subject-error"),
+  message: document.getElementById("message-error"),
+};
+
+const successMessage = {
+  name: document.getElementById("name-error"),
+  email: document.getElementById("email-error"),
+  subject: document.getElementById("subject-error"),
+  message: document.getElementById("message-error"),
+};
+
+validators = {
+  name: (value) => {
+    if (!value) return "Name is required";
+    if (value.length < 2) return "Name must be at least two characters";
+  },
+};
+
+const keys = Object.keys(formInput);
+// console.log(keys)
+
+keys.forEach((key) => {
+  let input = formInput[key];
+
+  input.addEventListener("input", () => {
+    let error = validators[key](input.value);
+    if (error) {
+      formErrors[key].classList.add("invalid");
+      formInput[key].classList.add('error')
+      formErrors[key].textContent = error;
+    }
+  });
+});
+contactBtn.addEventListener("click", () => {
+  modalOverlay.classList.add("show");
+  document.body.style.overflow = "hidden";
+});
+
+modalCloseBtn.addEventListener("click", () => {
+  modalOverlay.classList.remove("show");
+  document.body.style.overflow = "scroll";
+});
+
+// Testing
+// console.log(contactBtn);
+// console.log(filterList);
+// console.log(contactModal);
+// console.log(formInput.name);
+// console.log(formInput.email);
+// console.log(formInput.subject);
+// console.log(formInput.message);
+// console.log(formErrors.name);
+// console.log(formErrors.email);
+// console.log(formErrors.subject);
+// console.log(formErrors.message);
